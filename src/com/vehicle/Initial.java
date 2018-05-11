a0db034c2e0075da83bcc2091264de14720076f4
youml <youwendong@vip.qq.com>
2018-05-11 23:20:13 +0800

    删除注释

--- src/com/vehicle/Initial.java
+++ src/com/vehicle/Initial.java
@@ -3,7 +3,6 @@ package com.vehicle;
 public class Initial {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub        
         Vehicle bus = new Bus();
         Vehicle train = new Train();
         Vehicle aeroplane = new Aeroplane();
